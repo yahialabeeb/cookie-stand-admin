95ea9c7709882e319034ad420a5f2adf254c362e
Yahialabeeb <yahia.labeeb@gmail.com>
2021-12-21 01:26:43 +0200

    lab37

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import { useState } from 'react';
+
 export default function Home() {
   const [store, set_data] = useState([]);
   function storeHandler(event) {
@@ -7,9 +8,9 @@ export default function Home() {
 
     const saved = {
       location: event.target.location.value,
-      min_customers: event.target.min.value,
-      max_customers: event.target.max.value,
-      avg_cookies: event.target.avg.value,
+      minCustomers: event.target.min.value,
+      maxCustomers: event.target.max.value,
+      avgCookies: event.target.avg.value,
 
     }
     set_data(store => [...store, saved])
@@ -26,39 +27,39 @@ export default function Home() {
         <p>Cookies Stand Admin</p>
       </header>
 
-      <main className="px-40 rounded shadow-md pt-15 pb-5">
-
-        <form class="bg-green-300 shadow-md rounded px-20 pt-15 pb-2 mb-14" onSubmit={storeHandler}>
+      <main className="rounded shadow-md pt-15 pb-5">
+        <div className="bg-green-300 shadow-md rounded mx-40 px-4 pt-15 pb-2 mb-20">
           <h1 className='my-6 pt-5 text-2xl text-center'>Create Cookie Stand</h1>
+          <form onSubmit={storeHandler}>
 
-          <div className="flex" >
-            <label className="mx-1" >Location</label>
-            <input name="location" className="flex-auto" />
-          </div>
-
-
-          <div className="flex w-3.5/4 mx-auto my-5">
-            <div className="flex-col w-1/4">
-              <label>Min Customers per hour</label>
-              <input type='number' name="min" className="" />
+            <div className="flex" >
+              <label className="" >Location</label>
+              <input name="location" className="flex-auto" />
             </div>
 
 
-            <div className="flex-col w-1/4">
-              <label>Max Customers per hour</label>
-              <input type='number' name="max" className="" />
+            <div className="flex my-4">
+              <div className="flex-col w-1/4 my-1">
+                <label>Min Customers per hour</label>
+                <input type='number' name="min" className="" />
+              </div>
+
+
+              <div className="flex-col w-1/4 my-1">
+                <label>Max Customers per hour</label>
+                <input type='number' name="max" className="" />
+              </div>
+
+
+              <div className="flex-col w-1/4 my-1">
+                <label>Average Cookies per Sale</label>
+                <input type='number' name="avg" className="" />
+              </div>
+
+              <button className="w-1/4 bg-green-500 ">Create</button>
             </div>
-
-
-            <div className="flex-col w-1/4">
-              <label>Average Cookies per Sale</label>
-              <input type='number' name="avg" className="" />
-            </div>
-
-            <button className="w-1/5 bg-green-500 ">Create</button>
-          </div>
-        </form>
-
+          </form>
+        </div>
         <p className='text-center text-1xl my-5'>
           Report Table Coming Soon...
         </p>
@@ -71,7 +72,7 @@ export default function Home() {
         }           
       </main>
       
-      <footer className="items-center p-4 text-3xl bg-green-400" >
+      <footer className="items-center p-4 text-1xl bg-green-400" >
         <p>&copy; 2021 </p>
       </footer>  
     </div>
