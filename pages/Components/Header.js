ea9c61d1b50396c46515668cb2ce915cf9b262bb
Yahialabeeb <yahia.labeeb@gmail.com>
2021-12-21 20:54:02 +0200

    lab38

--- pages/Components/Header.js
+++ pages/Components/Header.js
@@ -2,7 +2,7 @@ export default function Header(){
     return (
         <header className="flex items-center justify-between py-6 bg-green-400">
             <p className="mx-5 text-3xl" >Cookies Stand Admin</p>
-            <button className="px-2 mx-5 bg-gray-100 rounded shadow-md 1xl ">Overview</button>
+            <button className="px-2 mx-5 bg-gray-100 rounded shadow-md">Overview</button>
         </header>
     )
 }
--- pages/Components/ReportTable.js
+++ pages/Components/ReportTable.js
@@ -1,13 +1,27 @@
+import data from ""../data.js"
 export default function ReportTable(props){
     return(
         <>
         {props.store.length ?
-            <h1>5</h1>
-            :
             <p className="flex items-center justify-center h-12 text-xl "	>
                 No Cookie Stand Available
             </p>
-
+            :
+            <table>
+                <thead>
+                    {for(let i=0 ; i < data.length;i++){
+                        i
+                    }
+                        
+                    }
+                </thead>
+                <tbody>
+                    
+                </tbody>
+                <tfoot>
+                    
+                </tfoot>
+            </table>
 
         }
         </>
