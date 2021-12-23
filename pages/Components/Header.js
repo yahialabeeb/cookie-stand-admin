5f5d04b15246b7a5a79637c475481dd2b41e7ad9
Yahialabeeb <yahia.labeeb@gmail.com>
2021-12-23 02:26:20 +0200

    lab39

--- pages/Components/Header.js
+++ pages/Components/Header.js
@@ -1,7 +1,11 @@
+import { useAuth } from '../../contexts/auth'
+
 export default function Header(){
+    const { logout } = useAuth();
     return (
         <header className="flex items-center justify-between py-6 bg-green-400">
             <p className="mx-5 text-3xl" >Cookies Stand Admin</p>
+            <button onClick={logout} className="px-2 mx-5 bg-gray-100 rounded shadow-md">Logout</button>
             <button className="px-2 mx-5 bg-gray-100 rounded shadow-md">Overview</button>
         </header>
     )
